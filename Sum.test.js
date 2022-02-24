763d1155b9e8f97c7a17da49b94e0235a3d5eb42
amcmullen <amcmullen9291@gmail.com>
2022-02-24 08:28:25 -0500

    changed test back to passing results

--- Sum.test.js
+++ Sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./Sum');
+const sum = require('./Sum'); //imports file info needed for testing
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);  //returns an "expectation object";
@@ -16,8 +16,8 @@ test('object assignment', () => {
   //.toEqual is recursive => checks all attributes of an object.;
 
   test('adding positive numbers is not zero', () => {
-    for (let a = 0; a < 10; a++) {
-      for (let b = 0; b < 10; b++) {
+    for (let a = 1; a < 10; a++) {
+      for (let b = 1; b < 10; b++) {
         expect(a + b).not.toBe(0);
       }
     }
