b476015b386451b06356163072d893170035166f
amcmullen <amcmullen9291@gmail.com>
2022-02-24 08:26:28 -0500

    example of a failed test

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Jest
 
-Jest unit testing. Documentation can be found at jestjs.io# Jest-Unit-Testing
+Jest unit testing. Documentation can be found at jestjs.io# 
+
+and https://jestjs.io/docs/expect
+
+
+an exmaple of a failed test: 
+ ✓ adds 1 + 2 to equal 3 (2 ms)
+  ✓ object assignment (1 ms)
+  ✕ adding positive numbers is not zero (3 ms)
+
+  ● adding positive numbers is not zero
+
+    expect(received).not.toBe(expected) // Object.is equality
--- Sum.test.js
+++ Sum.test.js
@@ -1,5 +1,24 @@
-const sum = require('./sum');
+const sum = require('./Sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(1, 2)).toBe(3);  //returns an "expectation object";
 });
+
+
+//keyword 'test'(description, funciton call { keyword 'expect'(example agrs)  matcher keyword 'tobe' (example solution)});
+
+test('object assignment', () => {
+    const data = {one: 1};
+    data['two'] = 2;
+    expect(data).toEqual({one: 1, two: 2});
+  });
+
+  //.toEqual is recursive => checks all attributes of an object.;
+
+  test('adding positive numbers is not zero', () => {
+    for (let a = 0; a < 10; a++) {
+      for (let b = 0; b < 10; b++) {
+        expect(a + b).not.toBe(0);
+      }
+    }
+  });
