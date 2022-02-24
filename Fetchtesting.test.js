3810feef71d62f2dec3b32fd7434eb26b0d5bea7
amcmullen <amcmullen9291@gmail.com>
2022-02-24 09:18:34 -0500

    failed promises

--- Fetchtesting.test.js
+++ Fetchtesting.test.js
@@ -6,3 +6,10 @@ test('description', () => {
   });
 
   //this fails bc there was not fetch. but its testing the promise/fetched data;
+
+
+  //the following is to make sure a fetch is not fullfilled;
+  test('the fetch fails with an error', () => {
+    expect.assertions(1);
+    return fetchData().catch(e => expect(e).toMatch('error'));
+  });
